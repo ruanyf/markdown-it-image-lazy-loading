4d4ce4ba27fe5ebed16cb72a719803a6382154d8
ruanyf <yifeng.ruan@gmail.com>
2023-12-19 19:51:59 +0800

    chore: remove redundant files

--- test.mjs
+++
@@ -1,8 +0,0 @@
-import markdownit from 'markdown-it';
-import lazy_loading from './index.mjs';
-
-const md = markdownit();
-
-md.use(lazy_loading);
-console.log(md.render(`![](example.png "image title")`));
-// <p><img src="example.png" alt="" title="image title" loading="lazy"></p>\n
